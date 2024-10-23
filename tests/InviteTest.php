6a7f6b5fc76634251a7fca20d911693e9190fdb8
Владислав <a12341234@MacBook-Pro.local>
2024-10-23 20:30:45 +0700

    Добавлен phpUnit

---
+++ tests/InviteTest.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+use Routers\Router;
+
+class InviteTest extends TestCase {
+    public function test_router() {
+        $invite = new Router();
+        $html = $invite->route( "http://localhost/" );
+        $pos= mb_strpos($html, "Сделать заказ");
+        $this->assertNotEquals(false, $pos);
+    }
+}
